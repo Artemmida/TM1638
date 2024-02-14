f601c65625a777c52135d0efd648dc6be96ff0b7
Pocco Maxa <131971424+Artemmida@users.noreply.github.com>
2024-02-14 13:32:05 +0300

    Update translator.sv

--- translator.sv
+++ translator.sv
@@ -21,24 +21,24 @@
 
 module translator(
 
-    input logic [3:0] input,
-    output logic [7:0] output
+    input logic [3:0] inputt,
+    output logic [7:0] outputt
 );
     
 always_comb
 begin
 case (input)
-    4'd0: output = 8'b1111_1100;
-    4'd1: output = 8'b0110_0000;
-    4'd2: output = 8'b1101_1010;
-    4'd3: output = 8'b1111_0010;
-    4'd4: output = 8'b0110_0110;
-    4'd5: output = 8'b1011_0110;
-    4'd6: output = 8'b1011_1110;
-    4'd7: output = 8'b1110_0000;
-    4'd8: output = 8'b1111_1110;
-    4'd9: output = 8'b1111_0110;
-    default: output = 8'b0000_0001;
+    4'd0: outputt = 8'b1111_1100;
+    4'd1: outputt = 8'b0110_0000;
+    4'd2: outputt = 8'b1101_1010;
+    4'd3: outputt = 8'b1111_0010;
+    4'd4: outputt = 8'b0110_0110;
+    4'd5: outputt = 8'b1011_0110;
+    4'd6: outputt = 8'b1011_1110;
+    4'd7: outputt = 8'b1110_0000;
+    4'd8: outputt = 8'b1111_1110;
+    4'd9: outputt = 8'b1111_0110;
+    default: outputt = 8'b0000_0001;
 endcase
 end
 endmodule
